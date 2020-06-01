1d959a2e3d26a83c8f733635ecf0e971dc4cf39c
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-01 00:57:49 -0500

    Add files via upload

---
+++ AP CompSci/Dog.java
@@ -0,0 +1,26 @@
+public class Dog {
+    //2.1 Classes, Class, and Class Constructors
+    private String name = "";
+    private int age=0;   
+    //private: anywhere outside the class can't get to the variable directly
+    Dog () {
+        System.out.println("I am a dog."); // This method is the constructor
+    }
+
+    Dog(String myName){
+        System.out.println("I am a dog.");
+        name = myName;
+        System.out.println("My name is " + name + ".");
+    }
+
+    //2.3 Calling a Void Method
+    public void bark() {
+        System.out.println(name + " barks...BARK!!!");   
+        //A value is not returned
+    }
+    //2.5 calling a Void Method with Parameters 
+    public void setName(String myName){
+        return myName;  
+    }
+
+}
