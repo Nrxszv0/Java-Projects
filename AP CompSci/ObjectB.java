3c2b688a6f7eb22516a407c97f988e358a8c2ae3
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-01 23:43:55 -0500

    Update ObjectB.java

--- AP CompSci/ObjectB.java
+++ AP CompSci/ObjectB.java
@@ -39,6 +39,41 @@ public class ObjectB {
         for(int i = fullName.length(); i>=1; i--) {
             System.out.println("fullName substring 0," + i+": " +fullName.substring(0,i));
         }
+        //If string does not exist or is not found in substring you will get a -1
+        System.out.println("fullName indexOf stefani: " + fullName.indexOf("stefani"));
 
+        //String Comparisons
+        System.out.println("fullName equals lastName: " + fullName.equals(lastName));
+        System.out.println("fullName compareTo lastName: "+ fullName.compareTo(lastName));
+        //If it comes before in alphebetical order it will be - if it is the same it will be 0 if it is after it will be +
+        
+        // Getting one letter from a string
+        // Use substring(index. index + 1)
+        System.out.println("fullName substring 6,7: " + fullName.substring(6,7));
+        System.out.println("fullName substring 7,8: " + fullName.substring(7,8));
+        
+        //2.8 Wrapper Classes: Integer and Double
+        //Wrapper Classes == Classes that wrap around a primitive. Like an integer or double
+        
+        Integer age = new Integer(15);
+        Double height = new Double(65.5);
+        
+        //int age =  15;
+        //double height =  65.5;
+        
+        //Get a value from a wrapper class
+        System.out.println("age: " + age.intValue());
+        System.out.println("height: " +height.doubleValue());
+        
+        //MIN_VALUE AND MAX_VALUE
+        System.out.println("Integer.MIN_VALUE: " + Integer.MIN_VALUE);
+        System.out.println("Integer.MAX_VALUE: " + Integer.MAX_VALUE +"\n");
+        
+        System.out.println("Double.MIN_VALUE: " + Double.MIN_VALUE);
+        System.out.println("Double.MAX_VALUE: " + Double.MAX_VALUE);
+        
+        
+        
+        
     }
 }
