44b9969b5d33623e4d5a75f3bf1ada466fdcba8e
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-04 00:25:47 -0500

    Update Class.java

--- AP CompSci/Class.java
+++ AP CompSci/Class.java
@@ -5,6 +5,22 @@ public class Class {
       //Card
       //5.2 Constructors
       String name = new String("Bob Mould");
-      Card card = new Card();
+      Card card = new Card("King", "S", 10);
+      Card card2 = new Card("Queen", "H", 11);
+      
+      //5.3 Document your code with comments
+      //5.4 Accessor Methods- allows you to get a value from an object - Getters
+      System.out.println(card.getName());
+      System.out.println(card.getSuit());
+      System.out.println(card.getValue() + "\n");
+      System.out.println(card2.getName() + " " + card2.getSuit() + " " + card2.getValue());
+      
+      //5.5 Mutator Methods - Setters
+      // Dont do this:card.suit = "D"; 
+      card.setName("Ace");
+      card.setSuit("S");
+      card.setValue(1);
+      
+     
     }
 } 
