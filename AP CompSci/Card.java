6404f7ef6e9c36418619b14fc5578a0802d81dbd
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-03 23:58:34 -0500

    Update Card.java

--- AP CompSci/Card.java
+++ AP CompSci/Card.java
@@ -6,6 +6,12 @@ public class Card {
     public Card() {
         System.out.println("I am a new card.");
     }
+    public Card(String myName, String mySuit, int myValue) {
+        name = myName;
+        suit = mySuit;
+        value = myValue;
+        System.out.println("I am a " + name + " of " + suit + " with a value of " + value +".");
+    }
     
     
 }
