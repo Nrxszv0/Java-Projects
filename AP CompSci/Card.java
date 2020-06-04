3e37d3893252adef26a5a578cd7cea983b685115
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-04 00:25:26 -0500

    Update Card.java

--- AP CompSci/Card.java
+++ AP CompSci/Card.java
@@ -6,12 +6,34 @@ public class Card {
     public Card() {
         System.out.println("I am a new card.");
     }
+
     public Card(String myName, String mySuit, int myValue) {
         name = myName;
         suit = mySuit;
         value = myValue;
         System.out.println("I am a " + name + " of " + suit + " with a value of " + value +".");
     }
+    //5.4 Accessor Methods
+    public String getName() {
+        return name;
+    }
     
+    public String getSuit() {
+        return suit;
+    }
+    
+    public int getValue() {
+        return value;
+    }
+    //5.5 Mutator Methods
+    public void setName(String newName) {
+        name = newName;
+    }
+    public void setSuit(String newSuit) {
+        suit = newSuit;
+    }
+    public void setValue(int newValue) {
+        value = newValue;
+    }
     
 }
