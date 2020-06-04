fa8415959e59b5c6b47b8461fc1e65c5ccbaeabb
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-03 23:52:39 -0500

    Add files via upload

---
+++ AP CompSci/Card.java
@@ -0,0 +1,11 @@
+public class Card {
+    private String name;
+    private String suit;
+    private int value;
+    // 5.2 Constructors
+    public Card() {
+        System.out.println("I am a new card.");
+    }
+    
+    
+}
