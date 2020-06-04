2f26b7c5e893b21ce9b669f42cfa67ca86dbd81d
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-03 23:52:53 -0500

    Add files via upload

---
+++ Class.java
@@ -0,0 +1,10 @@
+public class Class {
+  public static void main(String[] args) {
+      //5.1 Classes define objects which have attributes and behaviors
+      //Class is a description of an object, a blueprint
+      //Card
+      //5.2 Constructors
+      String name = new String("Bob Mould");
+      Card card = new Card();
+    }
+} 
