0843355ef7822579629b71187170fab26c30f13e
Nrxszv0 <58677365+Nrxszv0@users.noreply.github.com>
2020-06-04 22:24:04 -0500

    Update Card.java

--- AP CompSci/Card.java
+++ AP CompSci/Card.java
@@ -12,28 +12,56 @@ public class Card {
         suit = mySuit;
         value = myValue;
         System.out.println("I am a " + name + " of " + suit + " with a value of " + value +".");
+        int x = 10;
+        System.out.println("x: " + x);
+        //Scope is only in this constructor. Can't be accessed from anywhere else.
+        //Not defined outside of method.
+        // X dissapears
     }
     //5.4 Accessor Methods
     public String getName() {
-        return name;
+        return name;        
     }
-    
+
     public String getSuit() {
         return suit;
     }
-    
+
     public int getValue() {
         return value;
     }
+
+    public String toString() {
+        return "Name: " + name + " Suit: " + suit + " Value: " + value;
+    }
+
     //5.5 Mutator Methods
     public void setName(String newName) {
         name = newName;
     }
+
     public void setSuit(String newSuit) {
         suit = newSuit;
     }
+
     public void setValue(int newValue) {
         value = newValue;
     }
-    
+    //5.6 Writing methods
+    public void printCard() {
+        System.out.println("+---+");
+        System.out.println("|" +suit+"--|");
+        System.out.println("|-"+name+"-|");
+        System.out.println("|--"+suit+"|");
+        System.out.println("+---+");
+    }
+    //5.7 Static Methods, attached to the class itself.
+    public static void printCard(String name, String suit){
+        System.out.println("+---+");
+        System.out.println("|" +suit+"--|");
+        System.out.println("|-"+name+"-|");
+        System.out.println("|--"+suit+"|");
+        System.out.println("+---+");
+    }
+
 }
